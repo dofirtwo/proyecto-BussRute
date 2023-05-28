e96eb296b96e23bb6c09df16639b2bb07e2e4801
Santiago <santiago19267@gmail.com>
2023-05-28 14:17:18 -0500

    interfazRutas
    vista de la interfaz de las rutas que tendran los usuarios :D

--- BussRute/BussRute/urls.py
+++ BussRute/BussRute/urls.py
@@ -23,4 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('inicio/',views.inicio),
+    path('visualizarRutas/',views.visualizarRutas),
 ]
--- BussRute/appBussRute/views.py
+++ BussRute/appBussRute/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def inicio(request):
     return render(request,"inicio.html")
+
+def visualizarRutas(request):
+    return render(request, "usuario/inicio.html")
