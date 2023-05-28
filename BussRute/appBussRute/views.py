1a6427759e6835b482a2bebd516089ac7608064a
Mauricio-Kta333 <mp990975@gmail.com>
2023-05-27 23:02:52 -0500

    Agregacion Url y Inicio Sesion

--- BussRute/appBussRute/views.py
+++ BussRute/appBussRute/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
 # Create your views here.
 def inicio(request):
     return render(request,"inicio.html")
+
+def inicioSesion(request):
+    return render(request,"inicioSesion.html")
