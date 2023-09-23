4e2f0a082ceb4239a9242fae554fca6a9366c569
SantiagoVargas <santiago19267@gmail.com>
2023-09-23 16:51:01 -0500

    +5 Pruebas

--- BussRute/appBussRute/tests.py
+++ BussRute/appBussRute/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client, override_settings
-from appBussRute.models import Usuario, Rol, Ruta, FavoritoRuta,Comentario, Usuario, Ruta
-from appBussRute.views import registrarseUsuario, enviarCorreo, generarCodigoVerificacion 
+from appBussRute.models import *
+from appBussRute.views import *
 from django.http import HttpResponse
 import hashlib
 from django.urls import reverse
@@ -8,8 +8,7 @@ from django.core import mail
 import re
 from bs4 import BeautifulSoup
 import pdb
-from appBussRute.views import *
-
+import json
 
 class TestInicioSesion(TestCase):
     def setUp(self):
@@ -258,3 +257,164 @@ class CerrarSesionTest(TestCase):
         response = self.client.get(reverse('cerrarSesion'))
         self.assertEqual(response.status_code, 302)  # Verificar que la redirección fue exitosa
         self.assertNotIn('usuario_id', self.client.session)  # Verificar que 'usuario_id' ya no está en la sesión
+
+class VisualizarRutasTestCase(TestCase):
+    def setUp(self):
+        self.ruta = Ruta.objects.create(
+            rutNumero=8,
+            rutPrecio='2400',
+            rutEmpresa='Coomotor',
+            rutEstado='A'
+        )
+
+    def test_visualizar_rutas(self):
+        # Realiza una solicitud GET a la vista
+        response = self.client.get("/visualizarRutas/")  # Reemplaza 'nombre_de_la_vista' con la URL de la vista
+
+        # Verifica que la vista responda con un código 200 (éxito)
+        self.assertEqual(response.status_code, 200)
+
+         # Consulta la lista de rutas desde la base de datos
+        rutas_db = Ruta.objects.all()
+
+        # Verifica que la longitud de la lista de rutas en el contexto sea igual a la de la base de datos
+        self.assertEqual(len(response.context['rutas']), len(rutas_db))
+
+        # Compara los campos de las rutas en el contexto con las de la base de datos
+        for i, ruta_context in enumerate(response.context['rutas']):
+            self.assertEqual(ruta_context.rutNumero, rutas_db[i].rutNumero)
+            self.assertEqual(ruta_context.rutPrecio, rutas_db[i].rutPrecio)
+            self.assertEqual(ruta_context.rutEmpresa, rutas_db[i].rutEmpresa)
+            self.assertEqual(ruta_context.rutEstado, rutas_db[i].rutEstado)
+
+class EliminarFavoritoViewTest(TestCase):
+    def setUp(self):
+        self.usuario = Usuario.objects.create(
+        usuNombre="usuario_de_prueba",             
+        usuCorreo="usuario@example.com",
+        usuPassword="contra_de_prueba",
+        usuRol=Rol.objects.create(rolNombre='Usuario'))
+        self.ruta = Ruta.objects.create(rutNumero=8, rutPrecio='2400', rutEmpresa='Coomotor')
+        self.favorito = FavoritoRuta.objects.create(favRuta=self.ruta,favUsuario=self.usuario)
+        self.url ="/eliminarFavorito/"  # Asegúrate de ajustar el nombre de la URL según tu configuración
+
+    def test_eliminar_favorito_exitoso(self):
+        data = {'numeroRuta': self.ruta.rutNumero}
+        response = self.client.post(self.url, data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json()['mensaje'], "Ruta Favorita Eliminada")
+        response_data = response.json()
+        self.mensaje = response_data.get('mensaje', '')
+    def tearDown(self):
+        # Imprime el mensaje
+        print(self.mensaje)
+                  
+class TestRegistroFavorito(TestCase):
+    def setUp(self):
+        #Crea un usuario de prueba
+        self.client = Client()
+        self.usuario = Usuario.objects.create(usuNombre='usuario_de_prueba', usuCorreo='usuario@example.com', usuPassword='contra_de_prueba')
+        self.ruta = Ruta.objects.create(rutNumero=8, rutPrecio='2400', rutEmpresa='Coomotor')
+
+    def test_registro_favorito(self):
+        session = self.client.session
+        session['usuario_id'] = self.usuario.id  # Simular un usuario en sesión
+        session.save()
+        # Define los datos que enviarás en la solicitud POST
+        data = {
+            'ruta': self.ruta.rutNumero,
+        }
+
+        url = '/registroFavorito/'  # Ajusta la URL a tu vista
+        response = self.client.post(url, data)
+
+        # Verifica que la respuesta sea exitosa (código de respuesta HTTP 200)
+        self.assertEqual(response.status_code, 200)
+        # Analiza el contenido de la respuesta JSON
+        response_data = response.json()
+        self.assertEqual(FavoritoRuta.objects.count(), 1)
+
+        # Verifica que el mensaje sea el esperado
+        self.assertEqual(response_data['mensaje'], "Ruta Añadida a Favorito Correctamente")
+        self.mensaje = response_data.get('mensaje', '')
+    def tearDown(self):
+        # Imprime el mensaje
+        print(self.mensaje)
+         
+class TestEliminarRuta(TestCase):
+    def test_eliminar_ruta_exitoso(self):
+        # Crear una instancia de Ruta para eliminar
+        ruta = Ruta.objects.create(rutNumero=8, rutPrecio='2400', rutEmpresa='Coomotor')
+        detalle = DetalleRuta.objects.create(detRuta=ruta, detLatitud=2.941819435241974, detLongitud=-75.28997301713491)
+        ubicacion = UbicacionRuta.objects.create(ubiRuta=ruta, ubiBarrio='Altico', ubiComuna='Comuna 4', ubiSitioDeInteres='Hospital')
+
+        # Define los datos que enviarás en la solicitud POST
+        data = {
+            'id': ruta.id,
+        }
+
+        url = '/eliminarRuta/'  # Esta es la URL real de tu vista
+        response = self.client.post(url, data)
+
+        # Verifica que la respuesta sea exitosa (código de respuesta HTTP 200)
+        self.assertEqual(response.status_code, 200)
+
+        # Analiza el contenido de la respuesta JSON
+        response_data = json.loads(response.content.decode('utf-8'))
+
+        # Verifica que el mensaje sea el esperado
+        self.assertEqual(response_data['mensaje'], "Ruta Eliminada")
+
+        # Verifica que la ruta, detalle y ubicación se hayan eliminado correctamente
+        self.assertEqual(Ruta.objects.filter(id=ruta.id).count(), 0)
+        self.assertEqual(DetalleRuta.objects.filter(detRuta=ruta).count(), 0)
+        self.assertEqual(UbicacionRuta.objects.filter(ubiRuta=ruta).count(), 0)
+        # Analiza el contenido de la respuesta JSON
+        response_data = json.loads(response.content.decode('utf-8'))
+
+        # Verifica que el mensaje sea el esperado
+        self.assertEqual(response_data['mensaje'], "Ruta Eliminada")
+
+        self.mensaje = response_data.get('mensaje', '')
+
+    def tearDown(self):
+        # Imprime el mensaje
+        print(self.mensaje)
+         
+class TestRegistroRuta(TestCase):
+    def test_registro_ruta_exitoso(self):
+        # Define los datos que enviarás en la solicitud POST
+        data = {
+            'numeroRuta': 8,
+            'precio': '2400',
+            'empresa': 'Coomotor',
+            'detalle': json.dumps([
+                {'latitud': 2.941819435241974, 'longitud': -75.28997301713491},
+                {'latitud': 2.925534316101293, 'longitud': -75.2863676122053},
+                {'latitud': 2.9373159049785045, 'longitud': -75.26736857052656},
+            ]),
+            'ubicacion': json.dumps([{'barrio': 'Altico', 'comuna': 'Comuna 4', 'sitioDeInteres': 'Hospital'}])
+            }
+
+        url = '/registroRuta/'  # Esta es la URL real de tu vista
+        response = self.client.post(url, data)
+
+        # Verifica que la respuesta sea exitosa (código de respuesta HTTP 200)
+        self.assertEqual(response.status_code, 200)
+
+        # Verifica que se haya registrado la ruta correctamente en la base de datos
+        self.assertEqual(Ruta.objects.count(), 1)
+        self.assertEqual(DetalleRuta.objects.count(), 3)
+        self.assertEqual(UbicacionRuta.objects.count(), 1)
+        # Puedes agregar más aserciones para verificar otros aspectos de la respuesta si es necesario
+        # Analiza el contenido de la respuesta JSON
+        response_data = json.loads(response.content.decode('utf-8'))
+
+        # Verifica que el mensaje sea el esperado
+        self.assertEqual(response_data['mensaje'], "Se ha registrado la Ruta Correctamente")
+
+        self.mensaje = response_data.get('mensaje', '')
+
+    def tearDown(self):
+        # Imprime el mensaje
+        print(self.mensaje)
